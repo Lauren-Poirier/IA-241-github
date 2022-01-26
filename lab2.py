aca8c771f81d0b9e2d36476012b6aa07ecbe6676
EC2 Default User <ec2-user@ip-172-31-94-232.ec2.internal>
2022-01-26 21:38:15 +0000

    lec2

---
+++ lab2.py
@@ -0,0 +1,8 @@
+my_name = 'Tom'
+print (my_name.upper())
+my_id = '123'
+print (my_id)
+123 = my_id 
+my_id = your_id = 123
+cant assign to literal
+print(my_id_str. = ('123'))
---
+++ lec2.py
@@ -0,0 +1,18 @@
+"""
+this a regional comment
+"""
+print('hello world') #this is a line comment
+print (type ('123'))
+print (type(123.))
+print (type(123))
+print ("Tom's cat")
+print ('Toms cat')
+print ('hello world')
+print ('hello world' .upper())
+print('HELLO WORLD' .lower())
+print ('hello world' .split())
+print ('lauren poirier' .split())
+print('hello world'.split('o'))
+print(1**2)
+print(1+2)
+print('1.5(*)2')
